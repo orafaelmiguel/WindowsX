f51f5fc3fc66f89ea35b2ad71b597d469c82b59d
Rafael Miguel <rafaelmiguel0202@gmail.com>
2025-03-18 20:17:18 -0300

    fix: update drivers script for log support

--- powershell/update_drivers.ps1
+++ powershell/update_drivers.ps1
@@ -1,19 +1,22 @@
+. .\logs\drivers.ps1 
+
 Write-Output "Checking Drivers..."
+Write-Log "drivers.log" "Checking for driver updates..."
 
 if (!(Get-Module -ListAvailable -Name PSWindowsUpdate)) {
-    Write-Output "Installing PSWindowsUpdate module..."
-    Install-Module -Name PSWindowsUpdate -Force -AllowClobber
+  Write-Log "drivers.log" "Installing PSWindowsUpdate module..."
+  Install-Module -Name PSWindowsUpdate -Force -AllowClobber
 }
 
 Import-Module PSWindowsUpdate
 
-Write-Output "Enabling Windows Update for drivers..."
+Write-Log "drivers.log" "Enabling Windows Update for drivers..."
 Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
 
-Write-Output "Listing available driver updates..."
+Write-Log "drivers.log" "Listing available driver updates..."
 Get-WindowsUpdate -MicrosoftUpdate -Category "Drivers"
 
-Write-Output "Installing driver updates..."
+Write-Log "drivers.log" "Installing driver updates..."
 Get-WindowsUpdate -MicrosoftUpdate -Category "Drivers" -AcceptAll -AutoReboot
 
-Write-Output "Driver update process completed successfully."
+Write-Log "drivers.log" "Driver update process completed successfully."
