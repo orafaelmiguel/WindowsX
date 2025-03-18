eebc5b7463c5f6f99106a0038a60f07dc2f173f0
orafaelmiguel <rafaelmiguel0202@gmail.com>
2025-03-18 00:02:07 -0300

    Check XMP

---
+++ boot/check_xmp.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Checking XMP status..."
+
+XMP_STATUS=$(powershell.exe -NoProfile -ExecutionPolicy Bypass -Command \
+    "(Get-CimInstance -ClassName Win32_PhysicalMemory | Select-Object -ExpandProperty ConfiguredClockSpeed) -gt 0")
+
+if [[ "$XMP_STATUS" == "True" ]]; then
+    echo "XMP is enabled."
+else
+    echo "MP is disabled!. Enable it in BIOS for better performance and run script again."
+fi
