2d0ca0e7c02ca6796e71d1348f4e41c52295390a
Rafael Miguel <rafaelmiguel0202@gmail.com>
2025-03-19 16:33:38 -0300

    feat: Adjust CPU frequency and avoid clock oscillations

---
+++ intel/disable_oscillations_clock.ps1
@@ -0,0 +1,14 @@
+Write-Output "Stabilizing CPU clocks..."
+
+# disable c-state (can generate higher temperatures)
+$regKey = "HKLM:\SYSTEM\CurrentControlSet\Control\Power"
+Set-ItemProperty -Path $regKey -Name "CsEnabled" -Value 0
+Write-Output "Disabled CPU C-states to avoid frequency scaling oscillations."
+
+$processorKey = "HKLM:\SYSTEM\CurrentControlSet\Control\Power\ProcessorPerformance"
+Set-ItemProperty -Path $processorKey -Name "IdleEnable" -Value 0
+Write-Output "Disabled idle state frequency scaling to maintain stable CPU frequency."
+$cpuInfo = Get-WmiObject -Class Win32_Processor
+$cpuInfo | ForEach-Object { Write-Output "Current CPU frequency: $($_.CurrentClockSpeed) MHz" }
+
+Write-Output "CPU frequency scaling has been adjusted to avoid clock oscillations."
