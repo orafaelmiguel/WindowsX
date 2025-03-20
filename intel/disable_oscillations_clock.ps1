ac3c47f1f87e2e429862939e6fb6e7240e30a59e
Rafael Miguel <rafaelmiguel0202@gmail.com>
2025-03-20 01:13:04 -0300

    refactor: change logic for CPU energy and performance

--- intel/disable_oscillations_clock.ps1
+++ intel/disable_oscillations_clock.ps1
@@ -5,8 +5,8 @@ $regKey = "HKLM:\SYSTEM\CurrentControlSet\Control\Power"
 Set-ItemProperty -Path $regKey -Name "CsEnabled" -Value 0
 Write-Output "Disabled CPU C-states to avoid frequency scaling oscillations."
 
-$processorKey = "HKLM:\SYSTEM\CurrentControlSet\Control\Power\ProcessorPerformance"
-Set-ItemProperty -Path $processorKey -Name "IdleEnable" -Value 0
+powercfg -setacvalueindex SCHEME_CURRENT SUB_PROCESSOR PROCTHROTTLEMAX 100
+powercfg -setactive SCHEME_CURRENT
 Write-Output "Disabled idle state frequency scaling to maintain stable CPU frequency."
 $cpuInfo = Get-WmiObject -Class Win32_Processor
 $cpuInfo | ForEach-Object { Write-Output "Current CPU frequency: $($_.CurrentClockSpeed) MHz" }
